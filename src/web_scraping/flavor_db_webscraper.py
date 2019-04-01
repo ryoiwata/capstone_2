6a01c3765596671867c16e4ace62a6756dde01b1
ryoi360 <ryoi360@gmail.com>
2019-04-01 14:25:47 -0500

    webscraper that will scrape everything on an ingredient page

--- src/web_scraping/flavor_db_webscraper.py
+++ src/web_scraping/flavor_db_webscraper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-
+x = 0
 #Loops through each ingredient page of FlavorDB
 for num in range(0,973):
     print("Currently on page: {}".format(num))
@@ -14,7 +14,31 @@ for num in range(0,973):
     
     #Scrape the ingredient name
     ingredient_name = str(soup.find('h1', class_='text-primary text-capitalize').text).strip()
+
+    #Scrape the category name
+    category_name = str(soup.find('span', class_='text-capitalize').text).strip()
     
+    #Acess the table (https://stackoverflow.com/questions/23377533/python-beautifulsoup-parsing-table)
+    molecule_table = soup.find('table', id= 'molecules')
+    table_body = molecule_table.find('tbody')
+
+    #Access every row in the table 
+    
+    for row in table_body.find_all('tr'):
+        #Access molecule name, id, and flavor in each row
+        molecule_name = row.findAll('td')[0]
+        molecule_id = row.findAll('td')[1]
+        molecule_flavor = row.findAll('td')[2]
+
+
+        # print(molecule_name.text)
+        # print("hello")
+        # print(molecule_id.text)
+        # print("hello")
+        # print(molecule_flavor.text)
+        
     break
+       
+        
 
 
