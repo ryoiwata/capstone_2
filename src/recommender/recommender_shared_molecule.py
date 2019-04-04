c56c6226c3fdf2893220f522f8643a041601b647
ryoi360 <ryoi360@gmail.com>
2019-04-04 14:39:37 -0500

    simple random walk for recommender

--- src/recommender/recommender_shared_molecule.py
+++ src/recommender/recommender_shared_molecule.py
@@ -10,6 +10,9 @@ import networkx as nx
 #For using a pickled dictionary
 import pickle
 
+#For random walk
+import random 
+
 #Opening the pickled file
 #Needs to be opened in the recommender folder
 pickle_in = open("./../../data/graph/molecule_full_graph.pickle","rb")
@@ -19,6 +22,22 @@ pickled_G = pickle.load(pickle_in)
 
 #Converting a dictionary of dictionaries to a graph
 G = pickled_G
+# print(G.nodes())
+
+#random walk algorithm
+current_node = "Egg"
+for num in range(10):
+    number_of_edges = len(G[current_node])
+    random_number = random.randint(1, number_of_edges)
+    current_node = list(G[current_node])[random_number]
+    print(current_node)
+
+
+
+# print(G["Egg"])
+
 
 print(G.number_of_edges())
 print(G.number_of_nodes())
+
+
