eed3a5e843d59cd8068d49df5986861a1b559b5a
ryoi360 <ryoi360@gmail.com>
2019-04-07 13:12:28 -0500

    webscraping, was able to see if query returned anything or not

--- src/web_scraping/recipe_puppy_webscraper.py
+++ src/web_scraping/recipe_puppy_webscraper.py
@@ -23,14 +23,47 @@ pickle_in = open("./ingredient_full_list.pickle","rb")
 #Getting the dictionary from the pickle
 pickled_list = pickle.load(pickle_in)
 
+x = 1
 #looping through each ingredient from the list
 for ingredient in pickled_list:
     #name of the ingredient to query, formating for website query
     #lowercasing, replacing the spaces for pluses
     ing_name = "+".join(ingredient.split()).lower()
+    
+    #for testing purposes, remove afterwards
+    ing_name = "sadlfkj"
+
+    # to check progress of scraper
+    x += 1
+    print(x)
     print(ing_name)
 
+    
+    for num in range(1,10): # will need to incorporate looping through each page
+        # to check progress of scraper
+        print("page: {}".format(num))
+        url = "http://www.recipepuppy.com/?i={}&q={}&p={}".format(ing_name, ing_name, num)
+        print(url)
 
+        #access the page
+        recipe_puppy_page = requests.get(url)
+        soup = BeautifulSoup(recipe_puppy_page.text, 'html.parser')
+
+
+        try:
+            #See if the query exists, we are looking for the word "sorry" in text
+            query_result = soup.find('div', style='padding:5px').text.split()
+            
+            #Conditional to see if there are any queries, if not then the scraper will go to the next
+            #If the text contains "sorry, then we'll move on to the next ingredient"
+            if query_result[0].strip() == "Sorry":
+                print("I'm sorry")
+                break
+        except:
+            pass
+
+
+    break
 
 """
 
