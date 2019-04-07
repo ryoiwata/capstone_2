5412a2e86ce722116f869515c0d2a1156146ca9d
ryoi360 <ryoi360@gmail.com>
2019-04-07 13:31:19 -0500

    filtering out ingredients without any results

--- src/web_scraping/recipe_puppy_webscraper.py
+++ src/web_scraping/recipe_puppy_webscraper.py
@@ -23,89 +23,48 @@ pickle_in = open("./ingredient_full_list.pickle","rb")
 #Getting the dictionary from the pickle
 pickled_list = pickle.load(pickle_in)
 
-x = 1
+x = 0
 #looping through each ingredient from the list
-for ingredient in pickled_list:
+for ingredient in pickled_list[:5]:
     #name of the ingredient to query, formating for website query
     #lowercasing, replacing the spaces for pluses
     ing_name = "+".join(ingredient.split()).lower()
     
     #for testing purposes, remove afterwards
-    ing_name = "sadlfkj"
+    # ing_name = "gfdfj"
 
     # to check progress of scraper
     x += 1
     print(x)
     print(ing_name)
-
     
-    for num in range(1,10): # will need to incorporate looping through each page
-        # to check progress of scraper
-        print("page: {}".format(num))
-        url = "http://www.recipepuppy.com/?i={}&q={}&p={}".format(ing_name, ing_name, num)
-        print(url)
+    url = "http://www.recipepuppy.com/?i={}&q={}".format(ing_name, ing_name)
+    recipe_puppy_page = requests.get(url)
+    soup = BeautifulSoup(recipe_puppy_page.text, 'html.parser')
 
-        #access the page
-        recipe_puppy_page = requests.get(url)
-        soup = BeautifulSoup(recipe_puppy_page.text, 'html.parser')
+    try:
+        #See if the query exists, we are looking for the word "sorry" in text
+        query_result = soup.find('div', style='padding:5px').text.split()
+        
+        #Conditional to see if there are any queries, if not then the scraper will go to the next
+        #If the text contains "sorry, then we'll move on to the next ingredient"
+        if query_result[0].strip() == "Sorry":
+            print("No results here")
+            continue
+    except: #if query exists we'll loop through each page of the ingredients
+        num_result = soup.find('div', class_='searchStats')
+        """
+        MAKE SURE TO BE ABLE TO GET THE NUMBER OF RESULTS
+        """
+        for num in range(1,5): # will need to incorporate looping through each page
+                # to check progress of scraper
+                print("page: {}".format(num))
+                url = "http://www.recipepuppy.com/?i={}&q={}&p={}".format(ing_name, ing_name, num)
+                print(url)
 
-
-        try:
-            #See if the query exists, we are looking for the word "sorry" in text
-            query_result = soup.find('div', style='padding:5px').text.split()
-            
-            #Conditional to see if there are any queries, if not then the scraper will go to the next
-            #If the text contains "sorry, then we'll move on to the next ingredient"
-            if query_result[0].strip() == "Sorry":
-                print("I'm sorry")
-                break
-        except:
-            pass
-
-
-    break
-
-"""
-
-#see if there is anything on the page
-url = "http://www.recipepuppy.com/?i=sdf&q="
-recipe_puppy_page = requests.get(url)
-soup = BeautifulSoup(recipe_puppy_page.text, 'html.parser')
-text = soup.find("div", class_ = "right")
-print(text)
-
-"""
-
-# recipe_puppy_page = requests.get('http://www.recipepuppy.com/?i={}&q='.format(name))
-
-# x = 0 
-# for name in pickled_list:
-
-
+                #access the page
+                recipe_puppy_page = requests.get(url)
+                soup = BeautifulSoup(recipe_puppy_page.text, 'html.parser')
     
-#     try:
-#         #need to incorporate a for loop for each page num of ingredient 
-        
-#         if len(name.split()) >= 1:
-#             joined_name = "+".join(name.split())
-#             recipe_puppy_page = requests.get('http://www.recipepuppy.com/?i={}&q='.format(joined_name))
 
-#         elif len(name.split()) == 0:
-#             recipe_puppy_page = requests.get('http://www.recipepuppy.com/?i={}&q='.format(name))
-
-#         #Soup object accesses the HTML of the Request object
-#         soup = BeautifulSoup(recipe_puppy_page.text, 'html.parser')
-        
-#         #Goes through each recip in the page
-#         for recipe in soup.findAll('div', class_='result'):
-#             print("hello!")   
-
-#         print("It worked!")
-#     except:
-#         print("Couldn't quite work!")
-      
-#     #to keep track of progress
-#     x += 1
-#     print(x)
-#     break
 
