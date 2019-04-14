3a53c3dda6058dcef8c83f4c445d74685d597f1d
ryoi360 <ryoi360@gmail.com>
2019-04-14 11:08:04 -0500

    girvan newman tweak to incorporate weights

--- src/communities/communities.py
+++ src/communities/communities.py
@@ -104,7 +104,6 @@ def get_modularity(subgraphs, degrees, num_edges):
                 mod -= degrees[node1] * degrees[node2] / (2. * num_edges)
     return mod / (2. * num_edges)
 
-
 def find_communities_modularities(G, max_iter=None):
     """Return communities and modularities.
     Run Girvan-Newman algorithm on G and return communities & modularity at for each step.
@@ -126,23 +125,55 @@ def find_communities_modularities(G, max_iter=None):
     modularities = []
     partitions = []
     i = 0
-    my_bool = True
-    while nx.number_connected_components(G1) < 4:
-        if nx.number_connected_components(G1) == 2 and my_bool:
-            with open('./data/graph/community_girvan_newman.graph', 'wb') as file:
-                file.write(pickle.dumps(G1))
-                file.close()
-            my_bool = False    
-        i += 1
-        print("Iteration: ", i)
+    while G1.number_of_edges() > 0:
         subgraphs = nx.connected_component_subgraphs(G1)
         modularity = get_modularity(subgraphs, degrees, num_edges)
         modularities.append(modularity)
         partitions.append(list(nx.connected_components(G1)))
         girvan_newman_step(G1)
+        i += 1
         if max_iter and i >= max_iter:
             break
     return partitions, modularities
+
+# def find_communities_modularities(G, max_iter=None):
+#     """Return communities and modularities.
+#     Run Girvan-Newman algorithm on G and return communities & modularity at for each step.
+#     Parameters
+#     ----------
+#     G: networkx Graph object
+#     max_iter: int, (optional, default=None)
+#         Maximum number of iterations
+#     Returns
+#     -------
+#     partitions: list of partitions
+#         (each partition is a list of sets (communities))
+#         (each set contains node labels (strings))
+#     modularities: corresponding modularities for each partition
+#     """
+#     degrees = G.degree()
+#     num_edges = G.number_of_edges()
+#     G1 = G.copy()
+#     modularities = []
+#     partitions = []
+#     i = 0
+#     my_bool = True
+#     while nx.number_connected_components(G1) < 4:
+#         if nx.number_connected_components(G1) == 2 and my_bool:
+#             with open('./data/graph/community_girvan_newman.graph', 'wb') as file:
+#                 file.write(pickle.dumps(G1))
+#                 file.close()
+#             my_bool = False    
+#         i += 1
+#         print("Iteration: ", i)
+#         subgraphs = nx.connected_component_subgraphs(G1)
+#         modularity = get_modularity(subgraphs, degrees, num_edges)
+#         modularities.append(modularity)
+#         partitions.append(list(nx.connected_components(G1)))
+#         girvan_newman_step(G1)
+#         if max_iter and i >= max_iter:
+#             break
+#     return partitions, modularities
 
 if __name__ == '__main__':
     
