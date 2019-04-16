f76119b841926c6c752200930768016ab2b8a89f
ryoi360 <ryoi360@gmail.com>
2019-04-16 13:34:14 -0500

    made a graph based on recipes that have flavordb ingredients

--- src/all_functions.py
+++ src/all_functions.py
@@ -15,7 +15,7 @@ from itertools import combinations
 
 #PandaDF of ingredients and their associated flavor molecules
 #Opening the pickled file
-pickle_in = open("./data/ingredients/ingredient_only_pd.pickle", "rb")
+pickle_in = open("./data/pandas/flavorDB_pandas.pickle", "rb")
 #Getting the PandaDF from the pickle
 ingredient_only_pd = pickle.load(pickle_in)
 
@@ -54,36 +54,36 @@ def graph_based_on_shared_recipe_creator(pandas_df = recipe_puppy_pandas):
     #iterate through each recipe
     for index, row in pandas_df.iterrows():
         ingredient_list = set(row["recipe_ingredients"])
-        if ingredient_list <= 1:
+        if len(ingredient_list) <= 1:
             continue
+        
+        for ingredient in ingredient_list:
+
+            if ingredient not in G:
+                category = ingredient_only_pd.loc[ingredient_only_pd['ingredient'] == ingredient, 'category'].values[0]
+                G.add_node(ingredient)
+                G.node[ingredient]["ingredient_node"] = True
+                G.node[ingredient]["molecule_node"] = False
+                G.node[ingredient]["category"] = category
+                G.node[ingredient]["quantity"] = 1
+
+            else:
+                G.node[ingredient]["quantity"] += 1
+
 
         #generator of all the pairs of ingredients within a recipe
         for combo in combinations(ingredient_list, 2):
             
-
             #individual ingredient from the combonitation
             ingredient_1 = combo[0]
             ingredient_2 = combo[1]
 
             if ingredient_1 == ingredient_2:
                 continue
-            category_1 = ingredient_only_pd.loc[ingredient_only_pd['ingredient'] == ingredient_1, 'category']
-            category_2 = ingredient_only_pd.loc[ingredient_only_pd['ingredient'] == ingredient_2, 'category']
-
-            #making a node for each ingredient and seeting the node attribute
-            G.add_node(ingredient_1)
-            G.node[ingredient_1]["ingredient_node"] = True
-            G.node[ingredient_1]["molecule_node"] = False
-            G.node[ingredient_1]["category"] = category_1
-            G.add_node(ingredient_2)
-            G.node[ingredient_2]["ingredient_node"] = True
-            G.node[ingredient_2]["molecule_node"] = False
-            G.node[ingredient_2]["category"] = category_2
-
 
             #Adding edges if an ingredient is within the same recipe
             if G.get_edge_data(ingredient_1, ingredient_2) == None:
-                G.add_edge(ingredient_1, ingredient_2, weight = 1)
+                G.add_edge(ingredient, ingredient_2, weight = 1)
             else:
                 G[ingredient_1][ingredient_2]["weight"] += 1
     
@@ -97,17 +97,17 @@ def graph_based_on_ingredient_with_associated_flavor_molecule_creator(pandas_df 
     for index, row in pandas_df.iterrows():
 
         #name of the ingredient from the "rows" 
-        ingredient_1 = row["ingredient"]
+        ingredient = row["ingredient"]
         #category of the ingredient from the "rows"
         category = row["category"]
         #set of the ingredient from the "rows"
         set_of_molecules= row["set_molecules"]
 
         #creating an ingredient node and adding attributes
-        G.add_node(ingredient_1)
-        G.node[ingredient_1]["ingredient_node"] = True
-        G.node[ingredient_1]["molecule_node"] = False
-        G.node[ingredient_1]["category"] = category
+        G.add_node(ingredient)
+        G.node[ingredient]["ingredient_node"] = True
+        G.node[ingredient]["molecule_node"] = False
+        G.node[ingredient]["category"] = category
 
         # to keep track of what's going on
         for molecule in set_of_molecules:           
@@ -115,7 +115,7 @@ def graph_based_on_ingredient_with_associated_flavor_molecule_creator(pandas_df 
             G.add_node(molecule)
             G.node[molecule]["molecule_node"] = True
             G.node[molecule]["ingredient_node"] = False
-            G.add_edge(ingredient_1, molecule)
+            G.add_edge(ingredient, molecule)
     return G
 
 
@@ -127,24 +127,24 @@ def graph_based_on_shared_molecule_creator(pandas_df = ingredient_only_pd, min_i
     #iterate through each row of flavorDB 
     for index, row1 in pandas_df.iterrows():
         #ingredient name
-        ingredient_1 = row1["ingredient"]
+        ingredient = row1["ingredient"]
         #ingredient category
         category_1 = row1["category"]
         #ingredient molecules
         molecules_1 = row1["set_molecules"]
 
         #creating an ingredient node and adding attributes
-        G.add_node(ingredient_1)
-        G.node[ingredient_1]["ingredient_node"] = True
-        G.node[ingredient_1]["molecule_node"] = False
-        G.node[ingredient_1]["category"] = category_1
+        G.add_node(ingredient)
+        G.node[ingredient]["ingredient_node"] = True
+        G.node[ingredient]["molecule_node"] = False
+        G.node[ingredient]["category"] = category_1
 
         for index, row2 in pandas_df.iterrows():
             #ingredient name
             ingredient_2 = row2["ingredient"]
             
             #checks to see if ingredients are different
-            if ingredient_1 != ingredient_2:
+            if ingredient != ingredient_2:
                 #ingredient category
                 category_2 = row2["category"]
                 #ingredient molecules
@@ -165,7 +165,7 @@ def graph_based_on_shared_molecule_creator(pandas_df = ingredient_only_pd, min_i
                 #creating an edge if the two ingredients
                 #have at least a minimum ratio of intersection 
                 if intersection_ratio > min_intersection_ratio:
-                    G.add_edge(ingredient_1, ingredient_2, weight=intersection_ratio)
+                    G.add_edge(ingredient, ingredient_2, weight=intersection_ratio)
     return G
 
 """
@@ -180,16 +180,16 @@ def common_pair_analysis(ing1, ing2, pandas_df = ingredient_only_pd, graph_it = 
         #set of the ingredient from the "rows"
         set_mol= row["set_molecules"]
         #name of the ingredient from the "rows" 
-        ingredient_1 = row["ingredient"]
+        ingredient = row["ingredient"]
 
-        if ingredient_1 in [ing1, ing2]:
+        if ingredient in [ing1, ing2]:
             mol_list.append(set_mol)
             for molecule in set_mol:
-                demo_G.add_node(ingredient_1)
-                demo_G.node[ingredient_1]["ingredient_node"] = True
+                demo_G.add_node(ingredient)
+                demo_G.node[ingredient]["ingredient_node"] = True
                 demo_G.add_node(molecule)
                 demo_G.node[molecule]["molecule_node"] = True
-                demo_G.add_edge(ingredient_1, molecule)
+                demo_G.add_edge(ingredient, molecule)
     ingredient_nodes = nx.get_node_attributes(demo_G, 'ingredient_node').keys()
     molecule_nodes = nx.get_node_attributes(demo_G, 'molecule_node').keys()
     pos=nx.spring_layout(demo_G)
